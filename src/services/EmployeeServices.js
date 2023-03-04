61ee088e4ff35865bc5dae6c10eab9021ef097e3
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-04 00:46:42 +0000

    creating list of employees

--- src/services/EmployeeServices.js
+++ src/services/EmployeeServices.js
@@ -1,4 +1,10 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
+
+const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees"
 class Employeeservice{
+  getEmployees(){
+    return Axios.get(EMPLOYEE_API_BASE_URL);
+  }
 
 }
+export default new Employeeservice();
