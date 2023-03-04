420dd82d0641216e6f4b5ed98bf4ac6b2ad0e1a3
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-04 10:37:27 +0000

    styling home page

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -4,7 +4,11 @@ class HeaderComponent extends Component {
   render() {
     return (
       <div>
-        
+        <header>
+          <nav className='navbar navbar-extend-md navbar-dark bg-dark'>
+            <div> <a href='https://javaguides.net' className='navbar-brand'>Employee Management App</a> </div>
+          </nav>
+        </header>
       </div>
     );
   }
