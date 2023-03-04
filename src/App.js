c6226832a1fe1f1308564b15cfd5bc011dfc7999
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-04 15:40:17 +0000

    styling home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import ListEmployeeComponents from './components/ListEmployeeComponent';
 import './styles/style.css'
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
+    <div>
+      <HeaderComponent />
     <div className="container">
       <ListEmployeeComponents />   
+    </div>
+    <FooterComponent />
     </div>
   );
 }
