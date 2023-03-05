2921abd961a2528a56e16a6d5c418420d61c8849
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-05 15:30:10 +0000

    configuring router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import ListEmployeeComponents from './components/ListEmployeeComponent';
 import './styles/style.css'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 
