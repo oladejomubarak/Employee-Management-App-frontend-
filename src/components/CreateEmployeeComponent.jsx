e0ad7b7b8f6ec26b15dfbfbb24049c8a42d0d660
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-06 13:15:05 +0000

    styling add  employee method added

--- src/components/CreateEmployeeComponent.jsx
+++ src/components/CreateEmployeeComponent.jsx
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class CreateEmployeeComponent extends Component {
   render() {
     return (
-      <div>
-        <h1>Employee Form</h1>
+      <div className='container'>
+        <div className='row'>
+          <div className='card col-md-6 offset-md-3 offset-md-3'>
+            <h3 className='text-center'>Add employee</h3>
+            <div className='card-body'>
+            <form>
+
+            </form>
+            </div>
+          </div>
+        </div>
+        
       </div>
     );
   }
