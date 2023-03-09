7d7e1009503dee0d0c6d1fce0210f17b3743623d
oladejomubarak <oladejomubarakade@gmail.com>
2023-03-09 11:41:38 +0000

    update employee method completed

--- src/services/EmployeeServices.js
+++ src/services/EmployeeServices.js
@@ -1,7 +1,7 @@
 import axios, { Axios } from "axios";
 
 const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees"
-class Employeeservice{
+class EmployeeService{
   getEmployees(){
     return axios.get(EMPLOYEE_API_BASE_URL);
   }
@@ -18,4 +18,4 @@ class Employeeservice{
   }
 
 }
-export default new Employeeservice();
+export default new EmployeeService();
